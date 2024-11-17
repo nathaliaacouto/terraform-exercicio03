5ee0b4295f519b5ce654d6ac6c6c121325de62ff
nathaliaacouto <nvc@cesar.school>
2024-11-17 16:41:04 -0300

    Atualizando nome dos dados

--- main.tf
+++ main.tf
@@ -11,46 +11,42 @@ module "sg"{
 }
 
 # Referenciar Subnet existente
-data "aws_subnet" "my_subnet" {
+data "aws_subnet" "mcc2_nvc_subnet" {
   id = "subnet-0f063c7a7ee981429"
 }
 
 # Referenciar Cluster EKS existente
-data "aws_eks_cluster" "my_eks" {
+data "aws_eks_cluster" "mcc2_nvc_eks" {
   name = "EKSDeepDive"
 }
 
 output "eks_cluster_endpoint" {
-  value = data.aws_eks_cluster.my_eks.endpoint
+  value = data.aws_eks_cluster.mcc2_nvc_eks.endpoint
 }
 
 output "eks_cluster_ca_certificate" {
-  value = data.aws_eks_cluster.my_eks.certificate_authority[0].data
+  value = data.aws_eks_cluster.mcc2_nvc_eks.certificate_authority[0].data
 }
 
-# Referenciar o Role que será usado pelo Node Group
 data "aws_iam_role" "node_role" {
   name = "EKSDeepDive_NodeGroup_Role_cdcp"
 }
 
 # Criar Node Group
 resource "aws_eks_node_group" "node_group_mcc2_nvc" {
-  cluster_name    = data.aws_eks_cluster.my_eks.name
+  cluster_name    = data.aws_eks_cluster.mcc2_nvc_eks.name
   node_group_name = "nodeGroup008"
   node_role_arn   = data.aws_iam_role.node_role.arn
-  subnet_ids      = [data.aws_subnet.my_subnet.id]
+  subnet_ids      = [data.aws_subnet.mcc2_nvc_subnet.id]
 
-  # Configuração do Node Group
   scaling_config {
     desired_size = 1
     max_size     = 2
     min_size     = 1
   }
 
-  # Definir tipo de instância
   instance_types = ["t3.small"]
 
-  # Security Groups (podem incluir o SG do Cluster e adicionais)
   ami_type = "AL2_x86_64" # Amazon Linux 2 para arquitetura x86
 
   tags = {
@@ -58,20 +54,20 @@ resource "aws_eks_node_group" "node_group_mcc2_nvc" {
   }
 }
 
-# Configuração do Provider Kubernetes (usando o EKS como cluster)
+# Configuração do Provider Kubernetes 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.my_eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.my_eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.my_eks.token
+  host                   = data.aws_eks_cluster.mcc2_nvc_eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.mcc2_nvc_eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.mcc2_nvc_eks.token
 }
 
 # Referenciar o Deployment de WordPress no ECR Público
 resource "kubernetes_deployment" "wordpress_deployment" {
   metadata {
-    name      = "wordpress"
+    name      = "wordpress-mcc2-nvc"
     namespace = "default"
     labels = {
-      app = "wordpress"
+      app = "wordpress-mcc2-nvc"
     }
   }
 
@@ -80,14 +76,14 @@ resource "kubernetes_deployment" "wordpress_deployment" {
 
     selector {
       match_labels = {
-        app = "wordpress"
+        app = "wordpress-mcc2-nvc"
       }
     }
 
     template {
       metadata {
         labels = {
-          app = "wordpress"
+          app = "wordpress-mcc2-nvc"
         }
       }
 
@@ -104,10 +100,9 @@ resource "kubernetes_deployment" "wordpress_deployment" {
   }
 }
 
-# Criar um Service para expor o Deployment
 resource "kubernetes_service" "wordpress_service" {
   metadata {
-    name      = "wordpress-service"
+    name      = "wordpress-service-mcc2-nvc"
     namespace = "default"
   }
 
@@ -126,6 +121,6 @@ resource "kubernetes_service" "wordpress_service" {
 }
 
 # Obter o token para autenticação com o cluster EKS
-data "aws_eks_cluster_auth" "my_eks" {
-  name = data.aws_eks_cluster.my_eks.name
+data "aws_eks_cluster_auth" "mcc2_nvc_eks" {
+  name = data.aws_eks_cluster.mcc2_nvc_eks.name
 }
--- vpc/main.tf
+++ vpc/main.tf
@@ -1,4 +1,4 @@
 # Referenciar VPC existente
-data "aws_vpc" "my_vpc" {
+data "aws_vpc" "mcc2_nvc_vpc" {
   id = "vpc-04644f64fd1bd8b34"
 }
